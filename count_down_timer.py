6d8adc72238c5775134c969c8256f96e8849eafa
KorRyu3 <146335193+KorRyu3@users.noreply.github.com>
2024-01-21 11:46:28 +0900

    [add] 基本的なタイマー機能の完成

---
+++ count_down_timer.py
@@ -0,0 +1,92 @@
+# 設計: https://sizu.me/koromaru/posts/km8stwou1zss
+
+from threading import Thread, Event
+from datetime import datetime, timedelta
+from typing import Union, Tuple
+import time
+import math
+
+class CountDownTimer:
+    """タイマークラス"""
+    def __init__(self):
+        self.start_time: Union[None, datetime] = None
+        self.stop_event: Event = Event()
+        self.remaining_time: int = 0
+
+    def start(self, e) -> None:
+        """タイマーのスタート"""
+        if self.start_time is None:
+            self.start_time = datetime.now()
+            self.stop_event.clear()
+            self.thread: Thread = Thread(target=self._run)
+            self.thread.start()
+
+    def stop(self, e) -> None:
+        """タイマーの停止"""
+        if self.start_time is not None:
+            self.stop_event.set()
+            self.thread.join()
+            self.remaining_time -= (datetime.now() - self.start_time).total_seconds()
+            self.start_time = None
+
+
+    def _run(self) -> None:
+        """タイマーの動作を実行"""
+        count = self.remaining_time
+        if self.stop_event.is_set():
+            count = self.remaining_time
+        while not self.stop_event.is_set():
+            print(f'Elapsed Time: {self.get_remaining_time()[0]}:{self.get_remaining_time()[1]} seconds')
+            if count == 0:
+                self.start_time = None
+                break
+            time.sleep(1)
+            count -= 1
+
+    def reset(self, e) -> None:
+        """タイマーをリセット"""
+        self.stop(e)
+        self.set_timer(minutes=0, seconds=0)
+
+    # setter
+    def set_timer(self, minutes: int, seconds: int) -> None:
+        """
+        タイマーの秒数設定を初期化
+
+        :param minutes: 分
+        :param seconds: 秒
+        """
+        self.seconds: int = seconds
+        self.minutes: int = minutes
+        self.remaining_time = self.seconds + self.minutes * 60  # 秒数
+
+    # getter
+    def _get_datetime_date(self, time: datetime) -> Tuple[int, int, int]:
+        """datetimeオブジェクトの時間、分、秒を取得"""
+        return time.hour, time.minute, time.second
+
+    def get_estimated_time(self) -> Tuple[int, int, int]:
+        """タイマーの終了時刻を返す"""
+        if self.start_time is None:
+            return 00, 00, 00
+        else:
+            # datetime + timedeltaは、新しいdatetimeオブジェクトを返す
+            end_time = self.start_time + timedelta(seconds=self.remaining_time)
+            return self._get_datetime_date(end_time)
+
+    def get_remaining_time(self) -> Tuple[int, int]:
+        """タイマーの残り時間を返す"""
+        if self.start_time is None:
+            # raise ValueError('タイマーがスタートしていません')
+            return 00, 00
+        else:
+            time = math.ceil( self.remaining_time - (datetime.now() - self.start_time).total_seconds() )
+            return divmod(time, 60)
+
+if __name__ == '__main__':
+    timer = CountDownTimer()
+    timer.set_timer(0, 5)
+    timer.start()
+    print(timer.get_estimated_time())
+    time.sleep(5)
+    timer.stop()
--- flet_main.py
+++ flet_main.py
@@ -1,45 +1,215 @@
-import flet as ft
-import threading
 import time
+from datetime import datetime
+from threading import Thread
 
-def main(page: ft.Page):
+import flet as ft
+from flet import Page, Text, ElevatedButton, TextField, Row, Column, Container
+from count_down_timer import CountDownTimer
 
-    def update_time():
-        start_time = time.time()
-        max_time = int(minutes.value) * 60 + int(seconds.value)
-        while time.time() - start_time <= max_time:
-            if not running:
-                break
-            elapsed = time.time() - start_time
-            timer_text.value = f"経過時間: {int(elapsed // 60)}分 {int(elapsed % 60)}秒"
-            progress_bar.value = elapsed / max_time
+def main(page: Page):
+
+    class FletTimer(CountDownTimer):
+        """自作のタイマークラスをFletで使えるようにしたクラス"""
+        def _update_remaining_time(self):
+            minutes, seconds = self.get_remaining_time()
+            if minutes < 10:
+                minutes = f"0{minutes}"
+            if seconds < 10:
+                seconds = f"0{seconds}"
+            remaining_time.value = f"{minutes}:{seconds}"
+            remaining_time.update()
+
+        def start(self, e) -> None:
+            """タイマーのスタート"""
+            # any:""
+            if input_min.value != "" and input_sec.value == "":
+                input_sec.value = "0"
+            # "":any
+            if input_min.value == "" and input_sec.value != "":
+                input_min.value = "0"
+            # "": ""
+            if input_min.value == "" or input_sec.value == "":
+                error_message.value = "分と秒の両方を入力してください"
+                error_message.visible = True
+                error_message.update()
+                return
+            # 0:0
+            if int(input_min.value) == 0 and int(input_sec.value) == 0:
+                error_message.value = "0分0秒は入力できません"
+                error_message.visible = True
+                error_message.update()
+                return
+            if self.remaining_time == 0:
+                self.set_timer(int(input_min.value), int(input_sec.value))
+                user_input.visible = False
+            if self.start_time is None:
+                start_btn.disabled = True
+                stop_btn.disabled = False
+                reset_btn.disabled = False
+                error_message.visible = False
+                self.start_time = datetime.now()
+                self.stop_event.clear()
+                self.thread: Thread = Thread(target=self._run)
+                self.thread.start()
+            else:
+                error_message.value = "もう一度startボタンを押してください"
+                error_message.visible = True
+
+            end_time.visible = True
+            for i, time in enumerate([end_hour, end_min, end_sec]):
+                time.value = str(self.get_estimated_time()[i])
+                if int(time.value) < 10:
+                    time.value = f"0{time.value}"
             page.update()
-            time.sleep(1)
 
-        if running:
-            ft.MessageBox.show(page, "時間です！", "指定の時間になりました。")
+        def stop(self, e) -> None:
+            if self.start_time is None:
+                error_message.value = "タイマーがスタートしていません"
+                error_message.visible = True
+            else:
+                start_btn.disabled = False
+                stop_btn.disabled = True
+            page.update()
+            try:
+                super().stop(e)
+            except TypeError:
+                error_message.value = "もう一度stopボタンを押してください"
+                error_message.visible = True
+                error_message.update()
 
-    def start_timer(e):
-        global running
-        running = True
-        threading.Thread(target=update_time).start()
+        def _run(self) -> None:
+            """タイマーの動作を実行"""
+            count = self.remaining_time
+            if self.stop_event.is_set():
+                count = self.remaining_time
+            while not self.stop_event.is_set():
+                self._update_remaining_time()
+                print(count)
+                if count == 0:
+                    self.start_time = None
+                    self._open_dlg()
+                    break
+                time.sleep(1)
+                count -= 1
 
-    def stop_timer(e):
-        global running
-        running = False
+        def reset(self, e) -> None:
+            self.stop(e)
+            self.set_timer(minutes=0, seconds=0)
+            remaining_time.value = "00:00"
+            end_time.visible = False
+            user_input.visible = True
+            error_message.visible = False
 
-    running = False
+            start_btn.disabled = False
+            stop_btn.disabled = True
+            reset_btn.disabled = True
+            # update
+            page.update()
+
+            input_sec.focus()
+
+        def _open_dlg(self):
+            page.dialog = end_dlg
+            end_dlg.open = True
+            page.update()
+
+    # Fletのメインプログラム---------------------------------------------------
+
+    timer = FletTimer()
+
+    page.title = "Count Down Timer"
+
+
+    end_dlg = ft.AlertDialog(title=ft.Text("タイマーが終了しました。お疲れさまでした！", size=20), on_dismiss=timer.reset)
+
+    end_hour = Text(value="00", size=20)
+    end_min = Text(value="00", size=20)
+    end_sec = Text(value="00", size=20)
+    end_time = Row(
+        controls=[
+            Text("終了時刻: ", size=20),
+            end_hour,
+            Text(":", size=20),
+            end_min,
+            Text(":", size=20),
+            end_sec,
+        ],
+        alignment=ft.MainAxisAlignment.CENTER
+    )
+    end_time.visible = False
+
+    remaining_time = Text(value="00:00", size=50)
+
+    input_width = 75
+    input_height = 100
+    input_min = TextField(
+        label="分",
+        hint_text="0",
+        width=input_width,
+        height=input_height,
+        input_filter=ft.NumbersOnlyInputFilter(),
+        max_length=2,
+        on_submit=timer.start,
+    )
+    input_sec = TextField(
+        label="秒",
+        hint_text="0",
+        width=input_width,
+        height=input_height,
+        input_filter=ft.NumbersOnlyInputFilter(),
+        max_length=2,
+        on_submit=timer.start,
+        autofocus=True,
+    )
+    user_input = Row(
+        controls=[
+            Text("入力: ", size=20, width=input_width, height=75, text_align=ft.TextAlign.CENTER),
+            input_min,
+            Text(":", size=20, width=20, height=75, text_align=ft.TextAlign.CENTER),
+            input_sec,
+        ],
+        height=200,
+        alignment=ft.MainAxisAlignment.CENTER,
+        # 非表示
+        visible=True
+    )
+
+    btn_padding = 50
+    start_btn = ElevatedButton("Start", on_click=timer.start, style=ft.ButtonStyle(
+        shape=ft.CircleBorder(), padding=btn_padding
+    ))
+    stop_btn = ElevatedButton("Stop", on_click=timer.stop, disabled=True, style=ft.ButtonStyle(
+        shape=ft.CircleBorder(), padding=btn_padding
+    ))
+    reset_btn = ElevatedButton("Reset", on_click=timer.reset, disabled=True, style=ft.ButtonStyle(
+        shape=ft.CircleBorder(), padding=btn_padding
+    ))
+    count_down_btns = Row(
+        controls=[
+            start_btn,
+            stop_btn,
+            reset_btn,
+        ],
+        alignment=ft.MainAxisAlignment.CENTER,
+    )
+
+    error_message = Text()
+
+
+
     page.add(
-        ft.Text("分:"),
-        ft.TextField(id="minutes", width=50),
-        ft.Text("秒:"),
-        ft.TextField(id="seconds", width=50),
-        ft.Progress(id="progress_bar", value=0, width=200),
-        ft.Text(id="timer_text", text="経過時間: 0分 0秒"),
-        ft.Row([
-            ft.ElevatedButton("スタート", on_click=start_timer),
-            ft.ElevatedButton("ストップ", on_click=stop_timer)
-        ])
+        end_time,
+        Row(
+            controls=[
+                remaining_time
+            ],
+            height=100,
+            # 中央揃え
+            alignment=ft.MainAxisAlignment.CENTER,
+        ),
+        count_down_btns,
+        user_input,
+        error_message
     )
 
 if __name__ == "__main__":
